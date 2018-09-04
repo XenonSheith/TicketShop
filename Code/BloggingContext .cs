6ae9960bfa111d1df1c9140d5d62ce517b37e757
ali <ali@COM002>
2018-09-04 19:05:17 +0430

    readme addded

--- Code/BloggingContext .cs
+++ Code/BloggingContext .cs
@@ -11,6 +11,15 @@ namespace TicketShop.Code
         }
         public DbSet<Test> Test { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Test>().Property( i => i.Title).HasMaxLength(200);
+            // modelBuilder.Entity<Test>()
+            //     .HasOne(p => p.Title)
+            //     .WithMany(b => b.Posts)
+            //     .HasForeignKey(p => p.BlogId)
+            //     .HasConstraintName("ForeignKey_Post_Blog");
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=blogging.db");
@@ -20,7 +29,9 @@ namespace TicketShop.Code
 
     public class Test
     {
-        public int Id {get;set;}
-        public string Title {get;set;}
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string Code { get; set; }
     }
 }
---
+++ readme.txt
@@ -0,0 +1,2 @@
+ dotnet ef migrations add second
+  dotnet ef database update
